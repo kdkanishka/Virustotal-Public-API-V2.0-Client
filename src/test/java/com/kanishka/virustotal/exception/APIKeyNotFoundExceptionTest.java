eaf30fc449ff39f9093d568d461d1149c0f376d8
Kanishka Dilshan <kdkanishka@gmail.com>
2013-09-03 11:00:03 +0530

    added tests to test apikey not found exception

---
+++ src/test/java/com/kanishka/virustotal/exception/APIKeyNotFoundExceptionTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kanishka.virustotal.exception;
+
+import com.kanishka.virustotalv2.VirusTotalConfig;
+import com.kanishka.virustotalv2.VirustotalPublicV2;
+import com.kanishka.virustotalv2.VirustotalPublicV2Impl;
+import org.junit.Test;
+
+/**
+ *
+ * @author kdkanishka@gmail.com
+ */
+public class APIKeyNotFoundExceptionTest {
+    
+    public APIKeyNotFoundExceptionTest() {
+    }
+
+
+    @Test(expected = APIKeyNotFoundException.class)
+    public void testException() throws APIKeyNotFoundException {
+        //without setting api key
+        VirustotalPublicV2 virusTotalv2API=new VirustotalPublicV2Impl();
+    }
+    
+    @Test(expected = APIKeyNotFoundException.class)
+    public void testExceptionForEmptyKey() throws APIKeyNotFoundException {
+        //without setting api key
+        VirusTotalConfig.getConfigInstance().setVirusTotalAPIKey("");
+        VirustotalPublicV2 virusTotalv2API=new VirustotalPublicV2Impl();
+    }
+}
